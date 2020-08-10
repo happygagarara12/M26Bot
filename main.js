9377797a83de9f7d3a674c0fa72534acafa801ef
blablablasmart <46888825+blablablasmart@users.noreply.github.com>
2020-08-10 17:00:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ client.once("ready", () => {
 
 client.on("message", message =>{
     try{
-        if(!message.content.startsWith(prefix) || message.author.bot) return;
+        if(!message.content.startsWith(prefix)) return;
         const args = message.content.slice(prefix.length).split(/ +/);
         const command = args.shift().toLowerCase();
         if(command === "help"){
